3daed3d4e06ead2e9dda5fb90eecf89a45be43a9
MIKE ESQUIVEL <you@example.com>
2021-03-18 23:45:15 -0400

    added tests

--- tests.cpp
+++ tests.cpp
@@ -3,3 +3,14 @@
 #include "funcs.h"
 
 // add your tests here
+TEST_CASE("removeLeadingSpace")
+{
+    CHECK(removeLeadingSpace("       int pi = 3;  ") == "int pi = 3;  ");
+    CHECK(removeLeadingSpace("   while(int pi = 3") == "while(int pi = 3");
+}
+
+TEST_CASE("Testing countChar")
+{
+    CHECK(countChar("double main(){{{}}}", '{') == 3);
+    CHECK(countChar("}while(int i }}", '}') == 3);
+}
