c9324b9b1209356e9329268b9d86d1febd7f13b7
MIKE ESQUIVEL <you@example.com>
2021-03-18 22:57:43 -0400

    added remove function

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 #include "funcs.h"
+#include <string>
+#include <cctype>
+std::string removeLeadingSpace(std::string line) {
+	std::string answer;
+	bool condition = false;
+	for (int i = 0; i < line.size(); ++i) {
+		if (!isspace(line[i])) {
+			condition = true;
+		}
+		if (condition) {
+			answer += line[i];
+		}
+	}
+	return answer;
+}
 
-// add functions here
+
 
