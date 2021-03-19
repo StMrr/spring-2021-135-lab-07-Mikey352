d6f31019282e8b1e39941e7ac8e0486f009c72b6
MIKE ESQUIVEL <you@example.com>
2021-03-18 23:39:37 -0400

    added main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
-
+#include <string>
 
 #include "funcs.h"
 
-int main()
-{
-  return 0;
+int main() {
+	std::string line;
+	int counter = 0;
+	while (std::getline(std::cin, line)){
+		std::string answer;
+		if (removeLeadingSpace(line)[0] == '}'){
+			counter = counter - 1;
+		}
+		for (int i = 0; i < counter; ++i){
+			answer += '\t';
+		}
+		answer += removeLeadingSpace(line);
+		std::cout << answer << std::endl;
+		counter += countChar(line, '{');
+		if (removeLeadingSpace(line)[0] == '}'){
+			counter++;
+		}
+		counter -= countChar(line, '}');
+	}
 }
