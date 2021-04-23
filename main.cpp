cf02038fa6315ad9d4384096a70549b7cb99620e
StMrr <boldeagle175@gmail.com>
2021-04-23 12:03:29 -0400

    added quick comments

--- main.cpp
+++ main.cpp
@@ -5,16 +5,16 @@
 
 int main() {
 	std::string line;
-	int counter = 0;
+	int counter = 0; //initializing variable to keep track of number of tabs
 	while (std::getline(std::cin, line)){
 		std::string answer;
 		if (removeLeadingSpace(line)[0] == '}'){
-			counter = counter - 1;
+			counter = counter - 1; //
 		}
 		for (int i = 0; i < counter; ++i){
-			answer += '\t';
+			answer += '\t'; //tabs being added as necessary
 		}
-		answer += removeLeadingSpace(line);
+		answer += removeLeadingSpace(line); //adding line to string that previously had tabs added
 		std::cout << answer << std::endl;
 		counter += countChar(line, '{');
 		if (removeLeadingSpace(line)[0] == '}'){
